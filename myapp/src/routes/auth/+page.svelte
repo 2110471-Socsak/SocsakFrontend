6233c445b53ef707ef88ccc1f8773fde3f4c5ec3
saimkrua <k.satapornimp@gmail.com>
2024-04-09 20:42:05 +0700

    [ADD] ui auth

--- myapp/src/routes/auth/+page.svelte
+++ myapp/src/routes/auth/+page.svelte
@@ -1,2 +1,19 @@
-<h1 class="underline">Hello and welcome to my site!</h1>
-<div>TO DO: implement login/signup page</div>
+<div class="w-full flex-1 flex items-center justify-center">
+  <form
+    method="POST"
+    class="max-w-[488px] w-full p-8 m-8 bg-slate-800 flex flex-col gap-8 rounded-lg items-center"
+  >
+    <div class="w-full flex justify-center text-base">
+      <button type="button" class="px-4 py-2 text-slate-400"> Login </button>
+      <button type="button" class="px-4 py-2 text-blue-300 border-b-[1.5px] border-blue-300"> Sign Up </button>
+    </div>
+    <div class="text-white font-semibold text-[32px] leading-[48px]">Log In</div>
+    <input name="username" type="text" placeholder="Enter your username..." class="w-full px-4 py-3 bg-slate-600 rounded-lg placeholder-slate-200 placeholder:text-base" />
+    <input name="passworf" type="password" placeholder="Enter your password..." class="w-full px-4 py-3 bg-slate-600 rounded-lg placeholder-slate-200 placeholder:text-base" />
+    <div class="w-full flex justify-end">
+      <button type="submit" class="w-auto bg-blue-500 rounded-[4px] text-white px-4 py-2 text-base"
+        >Done</button
+      >
+    </div>
+  </form>
+</div>
