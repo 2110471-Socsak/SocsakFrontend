bcd8987efd6753039ba115d6dc846098355e256b
paychorn <Rujjirap@gmail.com>
2024-04-13 00:23:34 +0700

    [EDIT] Update side bar

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,56 +1,347 @@
 <script>
-  const defaultChecked = true;
-  const online = [
-    {"title": "Test",},
-    {"title": "Test",},
-    {"title": "Test",},
-    {"title": "Test",},
+  const privateChatList = [
+    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+     {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "7",
+      "username": "Jame",
+      "timestamp": "1459361875666"
+    },
+    {
+      "id": "8",
+      "username": "Jack",
+      "timestamp": "1459361875666"
+    },    {
+      "id": "6",
+      "username": "Joe",
+      "timestamp": "1459361875666"
+    },
+    
   ];
-  const count = online.length;
+  const groupChatList = [
+    {
+      "id": "1",
+      "title":"Test",
+      "memberCount": 10,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "2",
+      "title":"Test2",
+      "memberCount": 14,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "3",
+      "title":"Test3",
+      "memberCount": 2,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+    {
+      "id": "4",
+      "title":"Test4",
+      "memberCount": 100,
+      "timestamp": "1459361875666",
+    },
+
+  ];
+  const user = {
+    "userId": "Myro",
+    "timestamp": "1459361875666"
+  }
+  const chat = {
+  "id": "chat-id",
+  "title": "Test",
+  "messages": {
+    "1": {
+      "sender": "eclarke",
+      "message": "ok let's make this happen",
+      "timestamp": 1459361875337
+    },
+    "2": {
+      "sender": "eclarke",
+      "message": "ok let's make this happen",
+      "timestamp": 1459361875337
+    }
+  }
+}
 </script>
 
-<div class="bg-slate-900 w-screen h-screen p-10 flex">
-  <div class="w-3/12 h-full ml-0 p-4 flex flex-col justify-between">
-    <div>
-      <div class="text-white text-2xl font-semibold px-4">Socsak</div>
-      <div class="join join-vertical w-full">
-        <div class="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" checked=defaultChecked /> 
-          <div class="collapse-title text-sm font-medium flex gap-[6px]">
-            <div>Online</div>
-            <div>{count}</div>
-          </div>
-          <div class="collapse-content"> 
-            {#each online as item}
-            <div class="flex gap-4">
-              <div class="text-white p-1">{item.title}</div>
-            </div>
-            {/each}
-          </div>
-        </div>
-        <div class="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" /> 
-          <div class="collapse-title text-sm font-medium flex gap-[6px]">
-            <div>Group</div>
-            <div>{count}</div>
-          </div>
-          <div class="collapse-content"> 
-            {#each online as item}
-            <div class="flex gap-4">
-              <div class="text-white p-1">{item.title}</div>
-            </div>
-            {/each}
-          </div>
-        </div>
+<div class="bg-slate-900 w-screen h-screen flex m-0">
+  <div class="w-1/4 h-sreen mr-auto bg-slate-900 z-10 overflow-hidden flex flex-col justify-between">
+    <p class="sticky top-0 text-white text-2xl font-semibold p-8">Socsak</p>
+    <div class="w-full overflow-hidden flex flex-col px-4">
+      <div class="collapse collapse-arrow join-item overflow-hidden max-h-full min-h-14">
+        <input type="radio" name="my-accordion-4" checked=true /> 
+        <p class="sticky collapse-title text-sm font-medium flex gap-[6px]">
+          Chats <span>{privateChatList.length}</span>
+        </p>
+        <ul class="collapse-content overflow-scroll"> 
+          {#each privateChatList as item}
+          <li class="text-white p-1">{item.username}</li>
+          {/each}
+        </ul>
+      </div>
+      <div class="collapse collapse-arrow join-item overflow-hidden max-h-full min-h-14">
+        <input type="radio" name="my-accordion-4"/> 
+        <p class="sticky collapse-title text-sm font-medium flex gap-[6px] absolute">
+          Groups <span>{groupChatList.length}</span>
+        </p>
+        <ul class="collapse-content overflow-scroll"> 
+          {#each groupChatList as item}
+          <li class="text-white p-1">{item.title}</li>
+          {/each}
+        </ul>
       </div>
     </div>
-    <div class="text-slate-400 p-4">
-      Username:
-    </div>
+    <p class="sticky bottom-0 text-white bg-slate-900 p-8">
+      Username: <span>{user.userId}</span>
+    </p>
   </div>
-  <div class="w-full h-full p-4">
-    <div>
+  <div class="w-full h-full justify-between p-4 flex flex-col">
+    <header class="text-lg text-white h-12 inline-block align-middle">
       CP888 (10)
+    </header>
+    <div class="h-max">
+      <div class="chat chat-start text-pretty">
+        <div class="chat-bubble">It's over Anakin, <br/>I have the high ground.</div>
+      </div>
+      <div class="chat chat-end text-pretty	">
+        <div class="chat-bubble">You underestimate my power!</div>
+      </div>
     </div>
+    <input type="text" placeholder="Enter a message..." class="input input-bordered w-full h-12 m-0" />
   </div>
 </div>
