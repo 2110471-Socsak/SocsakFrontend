622868712ccc565660c4019380c4a77a33eb7666
paychorn <Rujjirap@gmail.com>
2024-04-21 15:54:38 +0700

    [EDIT] Remove unused code

--- myapp/src/routes/chat/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import Menu from '$routes/chat/+menu.svelte';
-  import MessageChannel from '$routes/chat/+message.svelte'; 
-</script>
-<div class="bg-slate-900 w-screen h-screen flex m-0">
-  <Menu />
-  <MessageChannel />
-</div>
