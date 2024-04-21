3c15692e58e829b7c1bc2df00cc1b048ac85ab85
paychorn <Rujjirap@gmail.com>
2024-04-21 21:58:42 +0700

    [Add] Chat

--- myapp/src/routes/message.svelte
+++ myapp/src/routes/message.svelte
@@ -1,3 +1,25 @@
 <script lang="ts" async>
   // const user = localStorage.getItem("user");
 </script>
+
+<div class="w-full h-full justify-between pl-4 pr-12 py-8 flex flex-col">
+  <header class="text-lg text-white h-12 inline-block align-middle">
+    CP888 (10)
+  </header>
+  <div class="h-max">
+    <div class="chat chat-start text-pretty">
+      <div class="chat-bubble">It's over Anakin, <br/>I have the high ground.</div>
+    </div>
+    <div class="chat chat-end text-pretty	">
+      <div class="chat-bubble">You underestimate my power!</div>
+    </div>
+  </div>
+  <div class="flex gap-2">
+    <input type="text" placeholder="Enter a message..." class="input input-bordered w-full h-12 m-0" />
+    <button class="w-8 h-8 ">
+      <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5.8335 11.6667L14.0002 3.5M14.0002 3.5L22.1668 11.6667M14.0002 3.5V24.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg> 
+    </button>
+  </div>
+</div>
